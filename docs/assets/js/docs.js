2037a6c86b8432ee74d84be292a87cdd0da7c1a2
msdx <645079761@qq.com>
2014-07-28 15:57:09 +0800

    Replace apis.google.com/js/plusone.js

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -460,7 +460,7 @@ false; // navigate across topic boundaries only in design docs
 
   //Loads the +1 button
   var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-  po.src = 'https://apis.google.com/js/plusone.js';
+  po.src = 'http://googleapiscdn.qiniudn.com/apis.google.com/js/plusone.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 
 
